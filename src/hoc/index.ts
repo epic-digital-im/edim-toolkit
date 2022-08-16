9babe98660df49de2f7ad591a5fef266732ce8ea
sthornock <spencer@thornock.net>
2022-08-16 08:36:13 -0700

    update module paths

--- src/hoc/index.ts
+++ src/hoc/index.ts
@@ -1,1 +1,2 @@
 export * from './ParseLiveQuery';
+export type { ParseLiveQueryProps } from './ParseLiveQuery';
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-export { default as FilterTable } from './parse-table/FilterTable';
-export { default as ControlledTable } from './parse-table/ControlledTable';
-export * from './parse-table';
 export * from './parse';
 export * from './hoc';
 export * from './components';
